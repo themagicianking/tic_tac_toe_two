ae6420b5179e22ed18391b69fb7fce15dd87170a
Thane Chaves <tmoonder@gmail.com>
2022-04-16 23:58:28 -0400

    Added a method to display the board and continued to figure out how best to store player selection data.

--- tic_tac_toe_two.rb
+++ tic_tac_toe_two.rb
@@ -1,3 +1,18 @@
+class Board
+  board_status = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9}
+
+  def display_board(board_status)
+    puts "  #{board_status[:a]} | b | c
+    --+---+--
+     d | e | f
+    --+---+--
+     g | h | i"
+  end
+end
+
+game = Board.new
+game.display_board(board_status)
+
 class Player
   def initialize
     puts "Hello! What is your name?"
@@ -12,6 +27,7 @@ class Player
 
   def turn
     puts "Which square would you like to mark?"
+    @choice = gets
     # gets player input
     # change whatever variable they picked to @symbol
   end
@@ -27,15 +43,8 @@ end
 # then the last player
 # then a new instance of the board would be generated
 
-class Board
-  def initialize(a, b, c, d, e, f, g, h, i)
-    # each letter variable is passed in from the players, maybe?
-  end
-  
-  def display
-    # board is displayed with each of the current variables?
-  end
-end
-
+puts "PLAYER ONE:"
 player_one = Player.new
+puts "PLAYER TWO:"
 player_two = Player.new
+player_one.display_board
