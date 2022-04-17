32572f2c98b4cd361b7698dacef3cd5f2b48be37
Thane Chaves <tmoonder@gmail.com>
2022-04-17 16:56:54 -0400

    Implemented basic function of passing the values of key value pairs into the tic tac toe grid.

--- tic_tac_toe_two.rb
+++ tic_tac_toe_two.rb
@@ -1,8 +1,5 @@
-class Board
-  def initialize
-  end
-
-  def board_display(a, b, c, d, e, f, g, h, i)
+module Board
+  def display(a, b, c, d, e, f, g, h, i)
     puts "    #{a} | #{b} | #{c}
     --+---+--
      #{d} | #{e} | #{f}
@@ -12,46 +9,37 @@ class Board
 end
 
 class Player
+  attr_reader :name, :symbol
+
   def initialize
     puts "Hello! What is your name?"
     @name = gets
     puts "What symbol would you like to use?"
     @symbol = gets
   end
-
-  def taken_spaces
-    @spaces = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  end
-
-  def turn
-    puts "Which square would you like to mark?"
-    @choice = gets
-    # gets player input
-    # change whatever variable they picked to @symbol
-  end
-
-  def win_status?
-    # check if the player meets winning conditions
-    # if "win", puts "you won!"
-    # if not, continue
-  end
 end
 
 class Game
+  include Board
+  attr_reader :player_one, :player_two, :tiles
+
   def initialize
+    puts "PLAYER ONE."
     @player_one = Player.new
+    puts "PLAYER TWO."
     @player_two = Player.new
-    @board = Board.new
   end
 
-  def display
-    @board.board_display(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  def tiles
+    tiles = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9}
+  end
+
+  def turn(player)
+    puts "#{player}, please enter the number of the square you would like to mark."
+    @choice = gets.to_i
   end
 end
 
-first_game = Game.new
-first_game.display
-
-# so the second to last player's filled spaces get pulled in
-# then the last player
-# then a new instance of the board would be generated
+new_game = Game.new
+new_game.display(new_game.tiles[:a], new_game.tiles[:b], new_game.tiles[:c], new_game.tiles[:d], new_game.tiles[:e], new_game.tiles[:f], new_game.tiles[:g], new_game.tiles[:h], new_game.tiles[:i])
+new_game.turn(new_game.player_one.name.to_s.chomp)
