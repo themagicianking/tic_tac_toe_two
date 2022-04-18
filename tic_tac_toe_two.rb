e61333a15ff00ed44747c69aa16f59319f8dfe7f
Thane Chaves <tmoonder@gmail.com>
2022-04-17 20:37:21 -0400

    Figured out how to read and write variables for the board display and made sure the display functioned correctly.

--- tic_tac_toe_two.rb
+++ tic_tac_toe_two.rb
@@ -9,6 +9,8 @@ module Display
 end
 
 class Board
+  attr_accessor :tiles
+
   def initialize
     @tiles = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9}
   end
@@ -19,19 +21,25 @@ class Board
 end
 
 class Player
-  attr_reader :name, :symbol
+  attr_reader :name, :symbol, :choice
+  attr_writer :choice
 
   def initialize
     puts "Hello! What is your name?"
-    @name = gets
+    @name = gets.chomp
     puts "What symbol would you like to use?"
     @symbol = gets
+  end
+
+  def turn
+    puts "#{name}, please enter the number of the square you would like to mark."
+    @choice = gets.to_i
   end
 end
 
 class Game
   include Display
-  attr_reader :player_one, :player_two, :board, :choice
+  attr_reader :player_one, :player_two, :board
 
   def initialize
     puts "PLAYER ONE."
@@ -40,13 +48,9 @@ class Game
     @player_two = Player.new
     @board = Board.new
   end
-
-  def turn(player, whose_turn)
-    puts "#{player}, please enter the number of the square you would like to mark."
-    @choice = gets.to_i
-  end
 end
 
 new_game = Game.new
-new_game.turn(new_game.player_one.name.to_s.chomp, new_game.player_one)
-new_game.board.update_score(new_game.choice, new_game.player_one.symbol)
+new_game.display(new_game.board.tiles[:a], 2, 3, 4, 5, 6, 7, 8, 9)
+new_game.player_one.turn
+new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol)
