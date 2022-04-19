dd43ecadb6a989af0ea2ce3d9ca758b4ef795293
Thane Chaves <tmoonder@gmail.com>
2022-04-19 09:07:59 -0400

    Constructing a quick way to get to the first turn when wins are possible so I can write a method to check for wins. I'll optimize this later.

--- tic_tac_toe_two.rb
+++ tic_tac_toe_two.rb
@@ -12,7 +12,7 @@ class Board
   attr_accessor :tiles
 
   def initialize
-    @tiles = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9}
+    @tiles = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9 }
   end
 
   def update_score(position, current_symbol, other_symbol)
@@ -35,19 +35,19 @@ class Board
     elsif position == @tiles[:i]
       @tiles[:i] = current_symbol
     else
-      puts "error!"
+      puts 'error!'
     end
   end
 end
 
 class Player
-  attr_reader :name, :symbol, :choice
-  attr_writer :choice
+  attr_reader :name, :symbol
+  attr_accessor :choice
 
   def initialize
-    puts "Hello! What is your name?"
+    puts 'Hello! What is your name?'
     @name = gets.chomp
-    puts "What symbol would you like to use?"
+    puts 'What symbol would you like to use?'
     @symbol = gets
   end
 
@@ -62,21 +62,74 @@ class Game
   attr_reader :player_one, :player_two, :board
 
   def initialize
-    puts "PLAYER ONE."
+    puts 'PLAYER ONE.'
     @player_one = Player.new
-    puts "PLAYER TWO."
+    puts 'PLAYER TWO.'
     @player_two = Player.new
     @board = Board.new
   end
 end
 
 new_game = Game.new
+
 new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-  new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-  new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+
 new_game.player_one.turn
-new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp, new_game.player_two.symbol.chomp)
-puts new_game.board.tiles
+
+new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp,
+new_game.player_two.symbol.chomp)
+
 new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-  new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-  new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+
+new_game.player_two.turn
+
+new_game.board.update_score(new_game.player_two.choice, new_game.player_two.symbol.chomp,
+new_game.player_one.symbol.chomp)
+
+new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
+new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+
+# look up that thingy that measures how many times a method has been called to use it on player one's turn and stop it after three turns to check win conditions but for now, quick n dirty coding
+
+new_game.player_one.turn
+
+new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp,
+new_game.player_two.symbol.chomp)
+
+new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
+new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+
+new_game.player_two.turn
+
+new_game.board.update_score(new_game.player_two.choice, new_game.player_two.symbol.chomp,
+new_game.player_one.symbol.chomp)
+
+new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
+new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+
+new_game.player_one.turn
+
+new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp,
+new_game.player_two.symbol.chomp)
+
+new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
+new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+
+=begin new_game.player_two.turn
+
+new_game.board.update_score(new_game.player_two.choice, new_game.player_two.symbol.chomp,
+new_game.player_one.symbol.chomp)
+
+new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
+new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])=end
+
+# insert method to check winning here
