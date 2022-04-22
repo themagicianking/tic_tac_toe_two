ee2b7e5e1e6c9ef9e927d451184510536676e237
Thane Chaves <tmoonder@gmail.com>
2022-04-22 14:25:10 -0400

    Game is fully funtional! May come back to clean up later.

--- tic_tac_toe_two.rb
+++ tic_tac_toe_two.rb
@@ -10,6 +10,7 @@ end
 
 class Board
   attr_accessor :tiles
+  attr_reader :update_score
 
   def initialize
     @tiles = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9 }
@@ -41,152 +42,99 @@ class Board
 end
 
 class Player
-  attr_reader :name, :symbol
+  attr_reader :name, :symbol, :turns_taken
   attr_accessor :choice
 
-  def initialize
+  def initialize(symbol)
     puts 'Hello! What is your name?'
     @name = gets.chomp
-    puts 'What symbol would you like to use?'
-    @symbol = gets
+    @symbol = symbol
   end
 
   def turn
     puts "#{name}, please enter the number of the square you would like to mark."
     @choice = gets.to_i
+    @turns_taken += @turns_taken
   end
 end
 
 class Game
   include Display
-  attr_reader :player_one, :player_two, :board
+  attr_reader :player_one, :player_two, :board, :game_over
 
   def initialize
     puts 'PLAYER ONE.'
-    @player_one = Player.new
+    @player_one = Player.new('x')
     puts 'PLAYER TWO.'
-    @player_two = Player.new
+    @player_two = Player.new('o')
     @board = Board.new
+    @game_over = 'no'
   end
 
   def win_check(player, symbol, tiles)
     if tiles[:a] == symbol && tiles[:b] == symbol && tiles[:c] == symbol
-      puts "#{player}, you win!"
+      puts "#{player.name}, you win!"
+      @game_over = 'yes'
     elsif tiles[:a] == symbol && tiles[:e] == symbol && tiles[:i] == symbol
-      puts "#{player}, you win!"
+      puts "#{player.name}, you win!"
+      @game_over = 'yes'
     elsif tiles[:a] == symbol && tiles[:d] == symbol && tiles[:g] == symbol
-      puts "#{player}, you win!"
+      puts "#{player.name}, you win!"
+      @game_over = 'yes'
     elsif tiles[:b] == symbol && tiles[:e] == symbol && tiles[:h] == symbol
-      puts "#{player}, you win!"
+      puts "#{player.name}, you win!"
+      @game_over = 'yes'
     elsif tiles[:c] == symbol && tiles[:f] == symbol && tiles[:i] == symbol
-      puts "#{player}, you win!"
+      puts "#{player.name}, you win!"
+      @game_over = 'yes'
     elsif tiles[:c] == symbol && tiles[:e] == symbol && tiles[:g] == symbol
-      puts "#{player}, you win!"
+      puts "#{player.name}, you win!"
+      @game_over = 'yes'
     elsif tiles[:d] == symbol && tiles[:e] == symbol && tiles[:f] == symbol
-      puts "#{player}, you win!"
+      puts "#{player.name}, you win!"
+      @game_over = 'yes'
     elsif tiles[:g] == symbol && tiles[:h] == symbol && tiles[:i] == symbol
-      puts "#{player}, you win!"
+      puts "#{player.name}, you win!"
+      @game_over = 'yes'
+    elsif player.turns_taken.to_i > 4
+      puts 'You tied!'
     else
-      # if turn checker has been going for long enough puts "it's a tie"
+      @game_over = 'no'
     end
   end
 end
 
 new_game = Game.new
 
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-new_game.player_one.turn
-
-new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp,
-new_game.player_two.symbol.chomp)
-
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-# look up that thingy that measures how many times a method has been called to use it on player one's turn and stop it after three turns to check win conditions but for now, quick n dirty coding
-
-new_game.player_two.turn
-
-new_game.board.update_score(new_game.player_two.choice, new_game.player_two.symbol.chomp,
-new_game.player_one.symbol.chomp)
+a = new_game.board.tiles[:a]
+b = new_game.board.tiles[:b]
+c = new_game.board.tiles[:c]
+d = new_game.board.tiles[:d]
+e = new_game.board.tiles[:e]
+f = new_game.board.tiles[:f]
+g = new_game.board.tiles[:g]
+h = new_game.board.tiles[:h]
+i = new_game.board.tiles[:i]
 
 new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
 new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
 new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
 
-new_game.player_one.turn
+while new_game.game_over == 'no' || new_game.player_one.turns_taken < 5
+  new_game.win_check(new_game.player_two, 'o', new_game.board.tiles)
+  break if new_game.game_over == 'yes' || new_game.player_one.turns_taken == 5
 
-new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp,
-new_game.player_two.symbol.chomp)
+  new_game.player_one.turn
+  new_game.board.update_score(new_game.player_one.choice, 'x', 'o')
+  new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
+  new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+  new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+  new_game.win_check(new_game.player_one, 'x', new_game.board.tiles)
+  break if new_game.game_over == 'yes' || new_game.player_one.turns_taken == 5
 
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-new_game.player_two.turn
-
-new_game.board.update_score(new_game.player_two.choice, new_game.player_two.symbol.chomp,
-new_game.player_one.symbol.chomp)
-
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-new_game.player_one.turn
-
-new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp,
-new_game.player_two.symbol.chomp)
-
-new_game.win_check(new_game.player_one.name, new_game.player_one.symbol.chomp, new_game.board.tiles)
-
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-new_game.player_two.turn
-
-new_game.board.update_score(new_game.player_two.choice, new_game.player_two.symbol.chomp,
-new_game.player_one.symbol.chomp)
-
-new_game.win_check(new_game.player_two.name, new_game.player_two.symbol.chomp, new_game.board.tiles)
-
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-new_game.player_one.turn
-
-new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp,
-new_game.player_two.symbol.chomp)
-
-new_game.win_check(new_game.player_one.name, new_game.player_one.symbol.chomp, new_game.board.tiles)
-
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-new_game.player_two.turn
-
-new_game.board.update_score(new_game.player_two.choice, new_game.player_two.symbol.chomp,
-new_game.player_one.symbol.chomp)
-
-new_game.win_check(new_game.player_two.name, new_game.player_two.symbol.chomp, new_game.board.tiles)
-
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-new_game.player_one.turn
-
-new_game.board.update_score(new_game.player_one.choice, new_game.player_one.symbol.chomp,
-new_game.player_two.symbol.chomp)
-
-new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
-new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
-new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
-
-puts "It's a tie!"
+  new_game.player_two.turn
+  new_game.board.update_score(new_game.player_two.choice, 'o', 'x')
+  new_game.display(new_game.board.tiles[:a], new_game.board.tiles[:b], new_game.board.tiles[:c],
+  new_game.board.tiles[:d], new_game.board.tiles[:e], new_game.board.tiles[:f],
+  new_game.board.tiles[:g], new_game.board.tiles[:h], new_game.board.tiles[:i])
+end
